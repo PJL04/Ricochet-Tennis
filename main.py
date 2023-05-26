724c33110f62f28ba543e025111d9e0457fffb08
PJL04 <134686620+PJL04@users.noreply.github.com>
2023-05-26 22:58:22 +0200

    Update main.py
    Added Rectangles and Background color as well as visuals (drawings)

--- main.py
+++ main.py
@@ -13,6 +13,17 @@ screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption("Ricochet Tennis")
 
 
+# Game Rectangles
+ball = pygame.Rect(screen_width/2 - 15, screen_height/2 - 15, 30, 30)
+player = pygame.Rect(screen_width - 20, screen_height/2 - 70, 10, 140)
+opponent = pygame.Rect(10, screen_height/2 - 70, 10, 140)
+
+# Background color
+bg_color = pygame.Color("grey12")
+light_grey = (200, 200, 200)
+
+
+# Game loop
 while True:
     # Handling input
     # get user input
@@ -23,6 +34,15 @@ while True:
             pygame.quit()
             # close window
             sys.exit
+
+    # Drawings
+    screen.fill(bg_color)
+    pygame.draw.rect(screen, light_grey, player)
+    pygame.draw.rect(screen, light_grey, opponent)
+    pygame.draw.ellipse(screen, light_grey, ball)
+    pygame.draw.aaline(screen, light_grey, (screen_width/2, 0), (screen_width/2,screen_height))
+    
+
 
     # Updating the window
     pygame.display.flip()
